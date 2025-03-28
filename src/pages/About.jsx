14b73e36799a3d908daefb182f1398530784fc65
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-03-29 00:13:19 +0300

    add react-router and create basic routing structure; remove unused CSS and add new page components

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam nihil dolores, commodi doloribus officia incidunt eaque ullam nisi illum ipsa, minus alias? Consequuntur voluptate
+                quae eligendi. Non laborum modi quam. Voluptatem illum numquam nisi expedita quod tenetur, tempora nobis nesciunt laudantium, facere in nemo iure dolorem, necessitatibus tempore aut
+                officiis cum incidunt. Veniam enim laboriosam impedit perspiciatis. A, fugit culpa!
+            </p>
+        </div>
+    );
+}
---
+++ src/pages/Courses.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Courses() {
+    return (
+        <div>
+            <h1>Courses</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et id illum nisi facere, labore doloremque unde quidem, eos accusantium qui maxime ipsum deleniti aperiam consequuntur, commodi
+                eligendi temporibus fugiat optio?
+            </p>
+        </div>
+    );
+}
+
+export default Courses;
