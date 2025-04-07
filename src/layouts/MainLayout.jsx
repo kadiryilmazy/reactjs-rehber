4950fb799e87ea364be675dc9cef6f41bc9a40df
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-07 20:42:52 +0300

    add Roboto font and ButtonUsage component; update MainLayout to include ButtonUsage; add MUI dependencies

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router";
+import ButtonUsage from "../components/ButtonUsage.jsx";
 
 export default function MainLayout() {
     return (
         <div className="container">
+            <ButtonUsage />
             <h1>MainLayout</h1>
             <Outlet />
         </div>
