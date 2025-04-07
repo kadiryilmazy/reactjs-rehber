9341aee9a286ff49c1bd14f50604fcdca43fd1ba
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-07 23:54:36 +0300

    refactor ProductList and MainLayout components; replace div with Grid2 for better layout and wrap Outlet in Container for consistent styling

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Grid2 from "@mui/material/Grid";
 
 export default function ProductList({ products }) {
     return (
-        <div>
+        <Grid2 container spacing={2}>
             {products.map((product) => (
-                <div key={product.id} className="product-item">
-                    <h2>{product.title}</h2>
-                </div>
+                <Grid2 sx={{ backgroundColor: "primary.light" }} size={{ xs: 6, md: 4, lg: 3 }} key={product.id}>
+                    <p>{product.title}</p>
+                </Grid2>
             ))}
-        </div>
+        </Grid2>
     );
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar.jsx";
+import { Container } from "@mui/material";
 
 export default function MainLayout() {
     return (
         <div className="container">
             <Navbar />
-            <Outlet />
+
+            <Container>
+                <Outlet />
+            </Container>
         </div>
     );
 }
