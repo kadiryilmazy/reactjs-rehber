2e701fdc67d7e09b9a18e7e52356e42d09547689
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-07 20:11:23 +0300

    add routing and layout for main application; implement pages for Cart, Home, Login, Products, Register, and ProductDetails; integrate react-router for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
-import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import { HomePage, ProductsPage, CartPage, LoginPage, RegisterPage, ProductDetailsPage } from "./pages/index.js";
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <MainLayout />,
+            children: [
+                {
+                    index: true,
+                    element: <HomePage />,
+                },
+                {
+                    path: "home",
+                    element: <HomePage />,
+                },
+                {
+                    path: "products",
+                    children: [
+                        {
+                            index: true,
+                            element: <ProductsPage />,
+                        },
+                        {
+                            path: ":id",
+                            element: <ProductDetailsPage />,
+                        },
+                    ],
+                },
+                {
+                    path: "cart",
+                    element: <CartPage />,
+                },
+                {
+                    path: "login",
+                    element: <LoginPage />,
+                },
+                {
+                    path: "register",
+                    element: <RegisterPage />,
+                },
+            ],
+        },
+    ]);
     return (
         <>
-            <h1>Hello</h1>
+            <RouterProvider router={router} />
         </>
     );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as CartPage } from "./Cart";
+export { default as HomePage } from "./Home";
+export { default as LoginPage } from "./Login";
+export { default as ProductsPage } from "./Products";
+export { default as RegisterPage } from "./Register";
+export { default as ProductDetailsPage } from "./ProductDetails";
