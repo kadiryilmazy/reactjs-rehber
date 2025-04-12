a29855deeeb52a96fb6d4bfcab2d720ba2f561a5
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-12 20:08:03 +0300

    add item handling to ProductItem and ProductDetail; implement add to cart functionality with loading state

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Grid2 from "@mui/material/Grid";
-import { Paper, Typography } from "@mui/material";
+import { Button, CircularProgress, Paper, Stack, Typography } from "@mui/material";
+import { currencyTRY } from "../utils/formats";
+import ReportIcon from "@mui/icons-material/Report";
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product, handleAddItem, cartItem, isAdding }) {
     return (
         <Grid2 container spacing={2} sx={{ marginTop: 3 }}>
             <Grid2 sx={{ backgroundColor: "primary.light" }} size={{ lg: 4, md: 5, sm: 6, xs: 12 }} key={product.id}>
@@ -16,6 +18,19 @@ export default function ProductItem({ product }) {
                         {product.title}
                     </Typography>
                     <Typography variant="body1">{product.description}</Typography>
+                    <Typography variant="h5" color="secondary" sx={{ mt: 3 }}>
+                        {currencyTRY.format(product.price)}
+                    </Typography>
+
+                    <Stack direction="row" display="flex" alignItems="center" gap={2} sx={{ mt: 3 }}>
+                        <Button variant="contained" color="secondary" sx={{ mt: 2 }} onClick={() => handleAddItem(product.id)}>
+                            Sepete Ekle
+                        </Button>
+                        <Typography variant="body2" color="text.secondary" sx={{ mt: 2, display: "flex", alignItems: "center" }}>
+                            <ReportIcon color="secondary" />
+                            Sepetinizde {cartItem?.product.quantity > 0 && !isAdding ? cartItem.product.quantity : <CircularProgress size="20px" sx={{ ml: 2 }} color="secondary" />} adet var.
+                        </Typography>
+                    </Stack>
                 </Paper>
             </Grid2>
         </Grid2>
