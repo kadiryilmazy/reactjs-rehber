6adf166ae3f9eec6053ec1be6d7bcd887ba5ff2e
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-12 15:18:19 +0300

    add item quantity management to CartPage; implement add and remove item functionality with loading state

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,35 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Button, CircularProgress, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { currencyTRY } from "../utils/formats";
 import { Delete } from "@mui/icons-material";
 import { useCartContext } from "../context/CartContext";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import { useState } from "react";
+import requests from "../api/apiClient";
 
 export default function CartPage() {
-    const { cart } = useCartContext();
+    const { cart, setCart } = useCartContext();
+    const [loading, setLoading] = useState(false);
+
     if (!cart || cart.cartItems.length === 0) return <Typography component="h4">Sepetinizde ürün yok</Typography>;
+
+    function handleAddItem(productId) {
+        setLoading(true);
+        requests.cart
+            .addItem(productId)
+            .then((cart) => setCart(cart))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false));
+    }
+
+    function handleRemoveItem(productId, quantity = 1) {
+        setLoading(true);
+        requests.cart
+            .deleteItem(productId, quantity)
+            .then((cart) => setCart(cart))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false));
+    }
 
     return (
         <TableContainer component={Paper}>
@@ -15,7 +39,7 @@ export default function CartPage() {
                         <TableCell sx={{ width: 100 }}></TableCell>
                         <TableCell>Ürün</TableCell>
                         <TableCell sx={{ width: 120 }}>Fiyat</TableCell>
-                        <TableCell sx={{ width: 120 }}>Adet</TableCell>
+                        <TableCell sx={{ width: 170 }}>Adet</TableCell>
                         <TableCell sx={{ width: 120 }}>Toplam</TableCell>
                         <TableCell sx={{ width: 50 }}></TableCell>
                     </TableRow>
@@ -28,7 +52,12 @@ export default function CartPage() {
                             </TableCell>
                             <TableCell>{item.product.title}</TableCell>
                             <TableCell>{currencyTRY.format(item.product.price)}</TableCell>
-                            <TableCell>{item.product.quantity}</TableCell>
+                            <TableCell>
+                                <Button onClick={() => handleAddItem(item.product.productId)}>{loading ? <CircularProgress size="20px" /> : <AddCircleOutlineIcon />}</Button>
+
+                                {item.product.quantity}
+                                <Button onClick={() => handleRemoveItem(item.product.productId)}>{loading ? <CircularProgress size="20px" /> : <RemoveCircleOutlineIcon />}</Button>
+                            </TableCell>
                             <TableCell>{currencyTRY.format(item.product.price * item.product.quantity)}</TableCell>
                             <TableCell>
                                 <IconButton color="error">
