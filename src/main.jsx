28dfd4ee633faa78a1dcd3121f8329952c279812
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-12 22:34:17 +0300

    integrate Redux Toolkit and React-Redux; set up store and wrap application with Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import "./index.css";
 import App from "./App.jsx";
 import React from "react";
 import { CartContextProvider } from "@/context/CartContext.jsx";
+import { Provider } from "react-redux";
+import { store } from "@/store/store.js";
 
 createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <CartContextProvider>
-            <App />
-        </CartContextProvider>
+        <Provider store={store}>
+            <CartContextProvider>
+                <App />
+            </CartContextProvider>
+        </Provider>
     </React.StrictMode>
 );
