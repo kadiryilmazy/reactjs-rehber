8125d052c34e39b4a7102d114fc9c638e5b7f754
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-13 00:49:06 +0300

    add counter slice and integrate with store; implement increment, decrement, and incrementByValue reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../pages/counter/counterSlice.js";
+
+if (!counterSlice || !counterSlice.reducer) {
+    throw new Error("counterSlice is not properly defined or missing a reducer property.");
+}
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterSlice.reducer,
+    },
 });
