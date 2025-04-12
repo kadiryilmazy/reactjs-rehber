2dfb67d4782c9193cb40b0eeeb99e7d6abe79c17
kadiryilmazy <kadiryilmazy@hotmail.com>
2025-04-12 20:16:48 +0300

    update CartPage to calculate and display subtotal, tax, and total; replace buttons with icon buttons for add/remove item actions

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,6 +10,10 @@ import requests from "../api/apiClient";
 export default function CartPage() {
     const { cart, setCart } = useCartContext();
     const [status, setStatus] = useState({ loading: false, id: "" });
+    const subTotal = cart?.cartItems.reduce((sub, item) => sub + item.product.price * item.product.quantity, 0);
+
+    const tax = subTotal * 0.2;
+    const total = subTotal + tax;
 
     if (!cart || cart.cartItems.length === 0) return <Typography component="h4">Sepetinizde ürün yok</Typography>;
 
@@ -33,14 +37,14 @@ export default function CartPage() {
     }
 
     return (
-        <TableContainer component={Paper}>
+        <TableContainer component={Paper} sx={{ margin: "auto", marginTop: 5 }}>
             <Table sx={{ minWidth: 650 }}>
                 <TableHead>
                     <TableRow>
                         <TableCell sx={{ width: 100 }}></TableCell>
                         <TableCell>Ürün</TableCell>
                         <TableCell sx={{ width: 120 }}>Fiyat</TableCell>
-                        <TableCell sx={{ width: 170 }}>Adet</TableCell>
+                        <TableCell sx={{ width: 180 }}>Adet</TableCell>
                         <TableCell sx={{ width: 120 }}>Toplam</TableCell>
                         <TableCell sx={{ width: 50 }}></TableCell>
                     </TableRow>
@@ -54,23 +58,47 @@ export default function CartPage() {
                             <TableCell>{item.product.title}</TableCell>
                             <TableCell>{currencyTRY.format(item.product.price)}</TableCell>
                             <TableCell>
-                                <Button onClick={() => handleAddItem(item.product.productId, "add" + item.product.productId)}>
-                                    {status.loading && status.id === "add" + item.product.productId ? <CircularProgress size="20px" /> : <AddCircleOutlineIcon />}
-                                </Button>
+                                <IconButton onClick={() => handleAddItem(item.product.productId, "add" + item.product.productId)}>
+                                    {status.id === "add" + item.product.productId && status.loading ? <CircularProgress size="20px" /> : <AddCircleOutlineIcon />}
+                                </IconButton>
 
                                 {item.product.quantity}
-                                <Button onClick={() => handleRemoveItem(item.product.productId, "remove" + item.product.productId)}>
-                                    {status.loading && status.id === "remove" + item.product.productId ? <CircularProgress size="20px" /> : <RemoveCircleOutlineIcon />}
-                                </Button>
+
+                                <IconButton onClick={() => handleRemoveItem(item.product.productId, "remove" + item.product.productId)}>
+                                    {status.id === "remove" + item.product.productId && status.loading ? <CircularProgress size="20px" /> : <RemoveCircleOutlineIcon />}
+                                </IconButton>
                             </TableCell>
                             <TableCell>{currencyTRY.format(item.product.price * item.product.quantity)}</TableCell>
                             <TableCell>
-                                <Button color="error" onClick={() => handleRemoveItem(item.product.productId, "remove_all" + item.product.productId, item.product.quantity)}>
-                                    {status.loading && status.id === "remove_all" + item.product.productId ? <CircularProgress size="20px" /> : <Delete />}
-                                </Button>
+                                <IconButton color="error" onClick={() => handleRemoveItem(item.product.productId, "remove_all" + item.product.productId, item.product.quantity)}>
+                                    {status.id === "remove_all" + item.product.productId && status.loading ? <CircularProgress size="20px" /> : <Delete />}
+                                </IconButton>
                             </TableCell>
                         </TableRow>
                     ))}
+
+                    <TableRow>
+                        <TableCell colSpan={5} align="right">
+                            <Typography>Ara Toplam</Typography>
+                        </TableCell>
+                        <TableCell>{currencyTRY.format(subTotal)}</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                        <TableCell colSpan={5} align="right">
+                            <Typography>KDV (20%)</Typography>
+                        </TableCell>
+                        <TableCell>{currencyTRY.format(tax)}</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                        <TableCell colSpan={5} align="right">
+                            <Typography>Genel Toplam</Typography>
+                        </TableCell>
+                        <TableCell>
+                            <Typography fontWeight="bold">{currencyTRY.format(total)}</Typography>
+                        </TableCell>
+                    </TableRow>
                 </TableBody>
             </Table>
         </TableContainer>
